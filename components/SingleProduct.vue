c53783c2f15a82517c0447915dfdfbad2a5cf11e
walkentalken <luke.wolck@gmail.com>
2018-07-24 11:53:11 -0400

    single product display data

--- components/SingleProduct.vue
+++ components/SingleProduct.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="single-product">
     <img :src="product.picture">
-    {{product.title}}
+    <div class="detail-container">
+      <h2>{{product.title}}</h2>
+      <span>{{product.company}}</span> | <span>{{product.color}}</span> | <span>{{product.price}}</span>
+      <p>{{product.about}}</p>
+    </div>
   </div>
 </template>
 
